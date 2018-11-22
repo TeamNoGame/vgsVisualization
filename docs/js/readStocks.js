0cdc2d7b07e559a3ec86ddf9dc2052577ce1bfa7
kiankoba <kiankoba@users.noreply.github.com>
2018-11-21 16:43:06 -1000

    Changed stock reading to be dynamic, renamed CSVs accordingly

---
+++ docs/js/readStocks.js
@@ -0,0 +1,58 @@
+// Read in each csv as a separate array
+let csvData = '';
+
+// TODO fix date parsing
+function readStocks(company) {
+  jQuery.get(`./data/${company}.csv`, function (txt) {
+    csvData = txt;
+  }).done(function () {
+    csvData = dataCleaning(csvData);
+    console.log("This is the cleaned data from the CSV", csvData);
+  });
+}
+
+// Make every row into an object and add it to the array
+function dataCleaning(dirtyData) {
+  var dirtyNames = '';
+  var dirtyObject = [];
+  dirtyData = dirtyData.split('\n');
+  if (dirtyData[0].includes(',')) {
+    dirtyNames = dirtyData[0].split(',');
+    dirtyData[0] = '';
+    dirtyData.shift();
+    for (var i = 0; i < dirtyData.length; i++) {
+      var tempObject = {};
+      for (var j = 0; j < dirtyNames.length; j++) {
+        tempObject[dirtyNames[j]] = dirtyData[i].split(',')[j];
+      }
+      dirtyObject.push(tempObject);
+    }
+  } else {
+    dirtyNames = dirtyData[0].split('\t');
+    dirtyData.shift();
+    for (var i = 0; i < dirtyData.length; i++) {
+      var tempObject = {};
+      for (var j = 0; j < dirtyNames.length; j++) {
+        tempObject[dirtyNames[j]] = dirtyData[i].split('\t')[j];
+      }
+      dirtyObject.push(tempObject);
+    }
+  }
+  return deString(dirtyObject);
+}
+
+// Parses fields from Strings into proper data types
+function deString(stringData){
+  for(let i = 0; i < stringData.length; i++){
+    let row = stringData[i];
+    row['Adj Close'] = parseFloat(row['Adj Close']);
+    row['Close'] = parseFloat(row['Close']);
+    row['Date'] = new Date(row['Date']);
+    row['Adj Close'] = parseFloat(row['Adj Close']);
+    row['High'] = parseFloat(row['High']);
+    row['Low'] = parseFloat(row['Low']);
+    row['Open'] = parseFloat(row['Open']);
+    row['Volume'] = parseInt(row['Volume']);
+  }
+  return stringData;
+}
